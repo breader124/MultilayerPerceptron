14afb6184ad6b9be2fa91bbca9593d89de45341f
Michał Wójcik <wojcikmichal98@gmail.com>
2020-06-07 13:53:59 +0200

    cleanup and minor bug fix

--- neural_network.py
+++ neural_network.py
@@ -73,7 +73,7 @@ class NeuralNetwork:
             sub_err.append(err)
 
             delta = output - yk
-            dqdw[-1] += delta * s[-1]
+            dqdw[-1] += np.outer(delta, s[-1])
             for k in reversed(range(1, len(dqdw))):
                 delta = self.weights[k] * delta * activation_derivative(s[k])
                 dqdw[k - 1] += np.outer(delta[:, :-1], s[k - 1])
@@ -162,73 +162,3 @@ class NeuralNetwork:
         y_raw = np.argmax(y, axis=1)
         errs = [a != b for a, b in zip(y_raw, maxed)]
         return 1 - (sum(errs) / len(y))
-
-    def debug_compute(self, input_data: Vector):
-        output_matrix = []
-        sum_matrix = []
-        for layer in self.weights:
-            layer_output = []
-            layer_sums = []
-            for neuron in layer:
-                summed = neuron[-1]
-                for i, weight in enumerate(neuron[:-1]):
-                    summed += input_data[i] * weight
-                activated = activation(summed)
-
-                layer_sums.append(summed)
-                layer_output.append(activated)
-
-            input_data = layer_output
-            sum_matrix.append(layer_sums)
-            output_matrix.append(layer_output)
-
-        return output_matrix, sum_matrix
-
-    def backward_propagation(self, output_matrix, sum_matrix,
-                             target_results, input_values):
-        derivatives = []
-
-        last_layer_der = []
-        for out, s, ideal in zip(output_matrix[-1], sum_matrix[-1],
-                                 target_results):
-            y_derivative = 2 * (out - ideal)
-            sum_derivative = y_derivative * activation_derivative(s)
-            res = {
-                'y': y_derivative,
-                's': sum_derivative
-            }
-            last_layer_der.append(res)
-        derivatives.append(last_layer_der)
-
-        for layer_index in reversed(range(len(self.weights) - 1)):
-            new_layer = []
-            for neuron_index in range(len(self.weights[layer_index])):
-                y_derivative = 0
-                for neuron_next_layer in range(
-                    len(self.weights[layer_index + 1])):
-                    neuron_sum = derivatives[-1][neuron_next_layer]['s']
-                    weight = self.weights[layer_index + 1][neuron_next_layer][
-                        neuron_index]
-                    y_derivative += neuron_sum * weight
-                sum_derivative = y_derivative * activation_derivative(
-                    sum_matrix[layer_index][neuron_index])
-                res = {
-                    'y': y_derivative,
-                    's': sum_derivative
-                }
-                new_layer.append(res)
-            derivatives.append(new_layer)
-
-        first_layer_der = []
-        for input_value_index in range(len(input_values)):
-            y_derivative = 0
-            for neuron_first_layer in range(len(self.weights[1])):
-                y_derivative += derivatives[-1][neuron_first_layer]['s'] * \
-                                self.weights[1][neuron_first_layer][
-                                    input_value_index]
-            first_layer_der.append(y_derivative)
-
-        return derivatives, first_layer_der
-
-    def train(self):
-        pass
